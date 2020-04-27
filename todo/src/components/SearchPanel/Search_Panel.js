8d70cc32e8dc75cab5f198ac011117258c3aa0aa
waiter-development <yatsunenko90@gmail.com>
2020-04-27 14:25:39 +0300

    add-state-underline-important

--- todo/src/components/SearchPanel/Search_Panel.js
+++ todo/src/components/SearchPanel/Search_Panel.js
@@ -1,20 +1,25 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 import ItemStatusFilter from '../ItemStatusFilter'
 
 import './Search_panel.css'
 
-const SearchPanel = () => {
+export default class SearchPanel extends Component {
+  
+  onInputBlur = () => {
+    console.log(this.props)
+  }
+  
+  render() {
 
-  const searchText = 'Type here to search'
-  
-  return (
-    <div className="search_panel">
-      <input className="search_panel_input" placeholder={searchText} />
-      < ItemStatusFilter/>
-    </div>
-  
-  )
+    const searchText = 'Type here to search'
+
+    return (
+      <div className="search_panel">
+        <input onBlur={this.onInputBlur} className="search_panel_input" placeholder={searchText} />
+        < ItemStatusFilter/>
+      </div>
+    
+    )
+  }
 }
-
-export default SearchPanel
--- todo/src/components/TodoListItem/Todo_List_Item.js
+++ todo/src/components/TodoListItem/Todo_List_Item.js
@@ -5,34 +5,57 @@ import './Todo_List_item.css'
 
 export default class TodoListItem extends Component {
 
+    state = {
+      done: false,
+      important: false
+    }
+  
+
   onLabelClick = () => {
-    console.log(`Done: ${this.props.label}`)
+    this.setState(({done}) => {
+        return {
+          done: !done
+        }
+    })
   }
 
-  
+  onImportantClick = () =>{
+    this.setState(({important}) => {
+      return  {
+        important: !important
+      }
+    })
+  }
+
     render() {
 
-      const { label, important = false } = this.props
-
-      const style = {
-        color: important ? 'steelblue' : 'black',
-        fontWeight: important ? 'bold' : 'normal'
+      const { label} = this.props
+      const {done, important} = this.state
+    
+      let classNames = 'todo_list_item'
+      if(done){
+        classNames += ' done'
       }
-      
+
+      if(important) {
+        classNames += ' important'
+      }
+
+
       return ( 
-        <span className="todo_list_item">
+        <span className={classNames}>
           <span className="todo_list_item_label" 
-                style={ style }
                 onClick={ this.onLabelClick } >
             { label }
           </span>
       
         <button type="button"
-                className="btn btn-outline-success btn-sm">
+                className="btn btn-outline-success btn-sm float-right"
+                onClick={this.onImportantClick}>
           <i className="fa fa-exclamation" />
         </button>
         <button type="button" 
-                className="btn btn-outline-danger btn-sm">
+                className="btn btn-outline-danger btn-sm float-right">
             <i className="fa fa-trash-o" />
         </button>
       
